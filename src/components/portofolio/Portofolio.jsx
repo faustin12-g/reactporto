10e8b06589f507a22be8a6fc60784d7e85619d48
faustin12-g <nshimiefaustinpeace@gmail.com>
2025-01-30 19:18:02 +0200

    propApp demo

--- src/components/portofolio/Portofolio.jsx
+++ src/components/portofolio/Portofolio.jsx
@@ -12,7 +12,7 @@ const data = [
     image:IMG1,
     title: 'Online Property Management',
     github:'https://github.com/Pelino-Courses/improved-property-management-system-with-ui-djangoninjas',
-    demo: 'https://github.com/Pelino-Courses/improved-property-management-system-with-ui-djangoninjas'
+    demo: 'https://www.veed.io/view/5f2a9d5a-abe9-4552-b88d-7b1f758bb828?panel=share'
 
   },
   {
