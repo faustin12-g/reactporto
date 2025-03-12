8a8e6a4cbfd907d087a7f1ba92af752ee1f264c8
faustin12-g <nshimiefaustinpeace@gmail.com>
2025-03-12 15:59:27 +0200

    same day commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,8 +15,8 @@ function Header() {
       <header>
         <div className="container header__container">
           <h5>Hello I'm</h5>
-          <h1>NSHIMIYIMANA Faustin</h1>
-          <h5 className='text-light'>Fullstack</h5>
+          <h1>NSHIMIYIMANA   Faustin</h1>
+          <h5 className='text-light'>Fullstack Developer</h5>
           <CTA />
           <HeaderSocial/>
 
