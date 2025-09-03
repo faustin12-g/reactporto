c0895431a3e39d92c711973f650271e71aee630a
faustin12-g <nshimiefaustinpeace@gmail.com>
2025-09-03 18:01:40 +0200

    change linkedIn

--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -5,7 +5,7 @@ import { FaXTwitter } from "react-icons/fa6";
 function HeaderSocial() {
   return (
     <div className='header__socials'> 
-    <a href="https://www.linkedin.com/in/faustin-nshimiyimana842/" target='_blank'><FaLinkedin/></a>
+    <a href="https://www.linkedin.com/in/faustin-nshimiyimana-928a6237b/" target='_blank'><FaLinkedin/></a>
     <a href="https://github.com/faustin12-g" target='_blank'><FaGithub/></a>
     <a href="https://x.com/Nshimiy58198795" target='_blank'><FaXTwitter/></a>
       
